9155d2582ee2f4f64fc0e4f285def4cfa39bb660
Katherine Patke <kmmpatke@gmail.com>
2021-12-09 19:00:14 -0600

    def not fully comprehending react hooks yet

--- src/components/Map.js
+++ src/components/Map.js
@@ -16,15 +16,54 @@ const TestMap = (props) => {
 
   const OtherTestMap = (props) => {
     const mapRef = useRef();
-  
-    useEffect(() => {
-      new window.google.maps.Map(mapRef.current, {
-        center: props.center,
-        zoom: props.zoom,
-      });
+    const [map, setMap] = useState();
+
+    var markerLatlng = {lng: -73.9217496,lat: 40.7649456};
+    var marker = Marker({
+        position: markerLatlng,
+        title:"Hello World!"
     });
+
+    useEffect(() => {
+        new window.google.maps.Map(mapRef.current, {
+          center: props.center,
+          zoom: props.zoom,
+        });
+      });
+    
+    useEffect(() => {
+        if (mapRef.current && !map) {
+          setMap(new window.google.maps.Map(mapRef.current, {}));
+        }
+      }, [mapRef, map]);
+    
+    // marker.setMap(mapRef.current);
   
     return <div ref={mapRef} id="map" />;
+  }
+
+  const Marker = (options) => {
+    const [marker, setMarker] = useState();
+
+    useEffect(() => {
+      if (!marker) {
+        setMarker(new window.google.maps.Marker());
+      }
+  
+      // remove marker from map on unmount
+      return () => {
+        if (marker) {
+          marker.setMap(null);
+        }
+      };
+    }, [marker]);
+  
+    useEffect(() => {
+      if (marker) {
+        marker.setOptions(options);
+      }
+    }, [marker, options]);
+  
   }
 
   export {TestMap, OtherTestMap};
